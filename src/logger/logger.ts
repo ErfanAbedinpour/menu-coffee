680e99c54eb9cd3a2d335bef55e4abba57cc2365
erfan <erfan.wtf44@gmail.com>
2025-03-25 16:29:14 -0400

    chore: Up^Cting Logger

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,25 +1,25 @@
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 
-const infoFileLogger = new winston.transports.File({
-  filename: `${process.cwd()}/logs/combine.log`,
-  level: 'info',
-});
+// const infoFileLogger = new winston.transports.File({
+//   filename: `${process.cwd()}/logs/combine.log`,
+//   level: 'info',
+// });
 const consoleLogger = new winston.transports.Console();
 
 const transports: winston.transport[] = [];
 
-if (process.env.NODE_ENV === 'production') {
-  transports.push(
-    new winston.transports.File({
-      filename: `${process.cwd()}/logs/error.log`,
-      level: 'error',
-    }),
-    infoFileLogger,
-  );
-} else {
-  transports.push(consoleLogger);
-}
+// if (process.env.NODE_ENV === 'production') {
+//   transports.push(
+//     new winston.transports.File({
+//       filename: `${process.cwd()}/logs/error.log`,
+//       level: 'error',
+//     }),
+//     infoFileLogger,
+//   );
+// } else {
+transports.push(consoleLogger);
+// }
 
 const logger = WinstonModule.createLogger({
   format: winston.format.combine(
